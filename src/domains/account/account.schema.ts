6657ad9a27be192b9f400afd02a414eee6aad6e8
Markos Mk <markosmk@me.com>
2026-03-27 15:14:24 -0300

    chore: formating

--- src/domains/account/account.schema.ts
+++ src/domains/account/account.schema.ts
@@ -4,11 +4,13 @@ export const updateMeSchema = v.object({
   name: v.optional(v.pipe(v.string(), v.minLength(2))),
   phone: v.optional(v.string()),
   ivaCategory: v.optional(v.string()),
-  profileInfo: v.optional(v.object({
-    companyName: v.optional(v.string()),
-    tradeName: v.optional(v.string()),
-    notes: v.optional(v.string())
-  }))
+  profileInfo: v.optional(
+    v.object({
+      companyName: v.optional(v.string()),
+      tradeName: v.optional(v.string()),
+      notes: v.optional(v.string())
+    })
+  )
 })
 
 export const changePasswordSchema = v.object({
