9ddfe236d50986447003bc628d2c570d475b3f2e
Markos Mk <markosmk@me.com>
2026-03-27 11:18:42 -0300

    add: config update listener

--- src/events/emitter.ts
+++ src/events/emitter.ts
@@ -5,6 +5,7 @@ export const appEvents = new EventEmitter()
 export const EventTypes = {
   USER_REGISTERED: "user.registered",
   PASSWORD_RESET_REQUESTED: "password.reset_requested",
-  USER_WELCOME: "user.welcome"
+  USER_WELCOME: "user.welcome",
+  MODULE_CONFIG_UPDATED: "module.config.updated"
   // ORDER_CREATED: "order.created",
 } as const
