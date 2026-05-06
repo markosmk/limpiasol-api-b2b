a614c470d78643900dba69d64f441664c9a4eac1
Markos Mk <markosmk@me.com>
2026-05-06 00:14:30 -0300

    fix: not use more mysql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is required")
+  throw new Error("DATABASE_URL environment variable is required");
 }
 
 export default defineConfig({
   schema: "./src/db/pg/*",
   out: "./drizzle/migrations",
-  dialect:"postgresql",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
